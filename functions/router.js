a21082e506dee494707212f8dbd649c5289b9100
Jacob Showman <jacob.showman@mrjaffesclass.com>
2022-11-29 12:49:29 -0800

    Working on getting it to run. First function coded in, but not recognized.

--- functions/router.js
+++ functions/router.js
@@ -1,40 +1,47 @@
 const router = require('express').Router();
-const nameSearch = require('./routes/nameSearch');
+const { byName, byYear } = require('us-baby-names');
 
 // Transform the data object elements into an
 // HTML table
+// Transform the data object elements into an
+// HTML table
 const formatToHTML = function(dataArr) {
-    // If dataArr is undefined or null, make an empty array
-    if (!dataArr) {
-      dataArr = [];
-    }
-    // Use the Array.map function to convert each record 
-    // into an HTML table element.
-    dataArr = dataArr.map(item => {
-      // Create the HTML here
-      let html = '<tr>'
-      html += (item.year) ? '<td>'+item.year+'</td>' : '';
-      html += (item.name) ? '<td>'+item.name+'</td>' : '';
-      html += (item.sex) ? '<td>'+item.sex+'</td>' : '';
-      html += (item.count) ? '<td>'+item.count+'</td>' : '';
-      html += '</tr>';
-      return html
-    })
-    // Now join all the elements together inside the 
-    // <table><tbody> elements.
-    return '<table><tbody>'+
-      dataArr.join('')+'</tbody></table>';
+  // If dataArr is undefined or null, make an empty array
+  if (!dataArr) {
+    dataArr = [];
   }
-  
-  // Transform name with first character capitalized and the 
-  // rest lower case
-  const fixName = function(name) {
-    let newName = name.toLowerCase();
-    newName = newName.charAt(0).toUpperCase() +
-      newName.substr(1)
-    return newName
-  }
+  // Use the Array.map function to convert each record 
+  // into an HTML table element.
+  dataArr = dataArr.map(item => {
+    // Create the HTML here
+    let html = '<tr>'
+    html += (item.year) ? '<td>'+item.year+'</td>' : '';
+    html += (item.name) ? '<td>'+item.name+'</td>' : '';
+    html += (item.sex) ? '<td>'+item.sex+'</td>' : '';
+    html += (item.count) ? '<td>'+item.count+'</td>' : '';
+    html += '</tr>';
+    return html
+  })
+  // Now join all the elements together inside the 
+  // <table><tbody> elements.
+  return '<table><tbody>'+
+    dataArr.join('')+'</tbody></table>';
+}
 
-router.get("/nameSearch", nameSearch);
+// Transform name with first character capitalized and the 
+// rest lower case
+const fixName = function(name) {
+  let newName = name.toLowerCase();
+  newName = newName.charAt(0).toUpperCase() +
+    newName.substr(1)
+  return newName
+}
+
+// Path 1: /baby-name/<name>
+router.get('/baby-name/:name', function(req, res) {
+  let data = byName[fixName(req.params.name)];
+  res.send(formatToHTML(data));
+})
+
 
 module.exports = router;
